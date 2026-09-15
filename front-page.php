6a5bcecb60ea9acd41f89cb8ad8ea26a18106a57
Opeyemi Oladejobi Akinkunmi <opeyemi@ezeroandone.io>
2026-09-15 18:15:50 +0100

    fix: add Master's prefix to International Tourism & Hospitality credential

--- front-page.php
+++ front-page.php
@@ -280,7 +280,7 @@ get_header(); ?>
                         <!-- Credentials badges -->
                         <div class="flex flex-wrap justify-center gap-2 mt-2">
                             <?php foreach ( [
-                                'Tourism & Hospitality',
+                                'Master\'s in Tourism & Hospitality',
                                 'MSc Business Management',
                                 'GSTC Trained',
                             ] as $cred ) : ?>
